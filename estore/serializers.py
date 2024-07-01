38c5a0729bfe019f799491e85e2fe3a2e7882bdd
victor-levus <95586250+victor-levus@users.noreply.github.com>
2024-07-01 20:55:59 +0100

    add category serializer

--- estore/serializers.py
+++ estore/serializers.py
@@ -1,18 +1,22 @@
 from rest_framework import serializers
 from estore.models import Product, Category
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description', 'image', 'createdAt',
+                  'updatedAt']
+
+
 class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+
     class Meta:
         model = Product
         fields = ['id', 'sku', 'name', 'description', 'category', 'price', 'image', 'createdAt',
                   'updatedAt']
         
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id', 'name', 'description', 'image', 'createdAt',
-                  'updatedAt']
         
 
     
