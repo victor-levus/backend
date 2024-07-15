cf7ced8e6a00aad71667201f40123de9859ffc64
victor-levus <95586250+victor-levus@users.noreply.github.com>
2024-07-15 05:27:10 +0100

    addedd CORS

--- database/settings/common.py
+++ database/settings/common.py
@@ -63,8 +63,8 @@ INTERNAL_IPS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://www.betcodes.space",
     "https://betcodes.space",
-    "http://betcodes.space",
 	"https://betsource-a3ca6.firebaseapp.com",
 	"https://betsource-a3ca6.web.app"
 ]
